b82143a547117b7b5daccad95ccbd19b36a3cfb0
sk9331657@gmail.com <sk9331657@gmail.com>
2019-03-10 01:05:54 +0530

    make it better

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ const express = require('express'),
 const hrgrouteshandler = require('./routes/hrg.route');
 mongoose.Promise = global.Promise;
 mongoose.connect(config.DB, { useNewUrlParser: true }).then(
-  () => { console.log('Database is connected') },
-  err => { console.log('Can not connect to the database' + err) }
 );
 
 
