3435bc1ac0c6fc85c45ead0f46b24daf19c36994
sk9331657@gmail.com <sk9331657@gmail.com>
2019-03-10 08:30:48 +0530

    make it better

--- routes/hrg.route.js
+++ routes/hrg.route.js
@@ -25,7 +25,7 @@ HRGRoutes.route('/gethospitals').get(function (req, res) {
           return value['data'];
       });
         
-          res.send({"data":newObject});
+          res.send(JSON.stringify(newObject));
         }
         });
         });
